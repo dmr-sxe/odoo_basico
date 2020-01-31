6c546e1479d89b791becd509aee29b364f503156
mirotic13 <dani-15-94@hotmail.com>
2020-01-31 14:29:03 +0100

    Añadido campo mes

--- models/informacion.py
+++ models/informacion.py
@@ -13,6 +13,7 @@ class informacion (models.Model):
     autorizado = fields.Boolean(string="¿Autorizado?", default=True)
     sexo_traducido = fields.Selection([('Hombre', 'Home'), ('Mujer', 'Muller'), ('Otros', 'Outros')], string='Sexo')
     data = fields.Date(string="Data", default=lambda self: fields.Date.today())
+    data_mes = fields.Text(compute="_mes_data", size=15, store=True)
     data_hora = fields.Datetime(string="Hora", default=lambda self: fields.Datetime.now())
     alto_en_cms = fields.Integer(string="Alto en centímetros")
     longo_en_cms = fields.Integer(string="Longo en centímetros")
@@ -61,5 +62,10 @@ class informacion (models.Model):
         for rexistro in self:
             if rexistro.peso < 1 or rexistro.peso > 4:
                 raise ValidationError('Os peso de %s ten que ser entre 1 e 4 ' % rexistro.name)
+
+    @api.depends('data')
+    def _mes_data(self):
+        for rexistro in self:
+            rexistro.data_mes = rexistro.data.strftime("%B")
 
 
